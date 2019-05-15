9c7beae7d8ee6ab2f60e03c9c05dd9f0086eb9ab
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-15 16:49:07 -0400

    along the righteous path

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 class Classic {
   constructor(frame, art_url) {
-    this.frame = frame("black-border")
-    this.art_url = art_url("https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg")
+    this.frame = "black-border"
+    this.art_url = "https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
   }
 }
+
+classic1 = new Classic("http://simpleabstract.com/assets/images/Oil-On-Canvas-Abstract-Art.jpg")

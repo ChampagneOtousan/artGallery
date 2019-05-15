1427c3a774d558635d3c03c97009be2776ee9b83
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-05-15 17:01:24 -0400

    ternary operator

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 class Classic {
-  constructor(frame, art_url) {
+  constructor(art_url) {
     this.frame = "black-border"
-    this.art_url = "https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg"
+    // thing_to_set_to =
+    this.art_url = art_url ? art_url : "default_url"
+  }
+
+  render() {
+    let el = document.createElement("div")
+    el.style.width = "100px"
+    el.style.height = "100px"
+    el.style.backgroundColor = "black"
+    document.body.appendChild(el)
   }
 }
 
